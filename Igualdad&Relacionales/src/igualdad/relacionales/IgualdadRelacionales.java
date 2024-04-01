166e2d17a8220b95b004c7055858cc96c32571cc
kony <yezzigarcia08@gmail.com>
2024-04-01 17:02:10 -0600

    modificacion_de_codigo

--- Igualdad&Relacionales/src/igualdad/relacionales/IgualdadRelacionales.java
+++ Igualdad&Relacionales/src/igualdad/relacionales/IgualdadRelacionales.java
@@ -17,12 +17,28 @@ public class IgualdadRelacionales {
         
         var cadena = "Hola";
         var cadena2 = "Adios";
-        var e = cadena == cadena2;
+        var e = cadena == cadena2;//comparar referencia de objetos
         System.out.println("e = " + e);
         
-        var f = cadena.endsWith(cadena2);
+        var f = cadena.endsWith(cadena2);//comparar contenido de cadenas
         System.out.println("f = " + f);
         
+        var g = a >= b;//mayor que >, o mayor o igual >=
+        System.out.println("g = " + g);
+        
+        if (a % 2 == 0){
+            System.out.println("es numero par");
+        } else{
+            System.out.println("es numero impar");
+        }
+        
+        var edad = 10;
+        var adulto = 18;
+        if (edad >= adulto){
+            System.out.println("es un adulto");
+        } else {
+            System.out.println("es menor de edad");
+        }
     }
     
 }
