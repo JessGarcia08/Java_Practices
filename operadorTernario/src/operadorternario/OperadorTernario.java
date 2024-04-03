e4d88fed35ce3b2701761299fec3318090561fd1
kony garcia <yezzigarcia08@gmai.com>
2024-04-03 03:19:02 +0100

    mas cursos

---
+++ operadorTernario/src/operadorternario/OperadorTernario.java
@@ -0,0 +1,42 @@
+
+package operadorternario;
+
+/**
+ *
+ * @author Missa
+ */
+public class OperadorTernario {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+   
+   
+public class Operadores_Condicionales {
+
+    public static void main(String[] args) {
+        var a = 8;
+        var valorMinimo = 0;
+        var valorMaximo = 10;
+
+        var resultado = a >= 0 && a <= 10;
+        if (resultado) {
+            System.out.println("dentro del rango");
+        } else {
+            System.out.println("Fuera de rango");
+        }
+        
+        var vacaciones = false;
+        var diaDescanso = false;
+        if (vacaciones || diaDescanso){
+            System.out.println("el padre puede asistir al juego");
+              } else {
+            System.out.println("el padre esta ocupado");
+        }
+    }
+
+}
+}
