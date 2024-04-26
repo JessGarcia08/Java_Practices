eaccb051e8381998de8c903a90f913b2f7e95bf6
kony <yezzigarcia08@gmail.com>
2024-04-25 20:31:33 -0600

    arrays_Java

--- arrays_Java/src/arrays_java/Arrays_Java.java
+++ arrays_Java/src/arrays_java/Arrays_Java.java
@@ -4,75 +4,52 @@ public class Arrays_Java {
 
     public static void main(String[] args) {
 
-        //aqui se prueba si x es mayor que y
-        int x = 20;
-        int y = 18;
-        if (x > y) {
-            System.out.println("x es mayor que y");
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        cars[0] = "Opel";//si el valor (cars[0]) permanece en 0 imprime opel
+        System.out.println(cars[0]);//modificando su valor imprime volvo,bmw,ford,mazda
+        // Now outputs Opel instead of Volvo
+
+        //en la matriz comienzan con 0 [0] el primer elemento [1] es el segundo elemento
+        String[] cars2 = {"Volvo", "BMW", "Ford", "Mazda"};
+        cars[0] = "opel";
+        System.out.println(cars2[1]);//el valor cars[0] si cambia imprime el nombre de las 4 
+        // Now outputs Opel instead of Volvo
+
+        //para saber cuantos elementos tiene una matriz
+        String[] cars3 = {"Volvo", "BMW", "Ford", "Mazda"};//modificando su valor numerico imprime volvo,bmw,ford,mazda
+        System.out.println(cars3.length);//length es para saber la propiedad
+        // Outputs 4
+
+        //matriz de array
+        String[] cars4 = {"Volvo", "BMW", "Ford", "Mazda"};
+        for (int i = 0; i < cars4.length; i++) {
+            System.out.println(cars4[i]);
         }
 
-        //Declaraciones con else
-        int time = 20;
-        if (time < 18) {//aqui el timpo (20) es mayor que 18
-            System.out.println("buenos dias");
-        } else {//como la condicion es falsa pasa difecto a else
-            System.out.println("buenas noches");
-        }
-        //declaraciones else if
-        int time1 = 22;
-        if (time1 < 10) {//aqui 22 es mayor que 10
-            System.out.println("buenos dias");
-        } else if (time1 < 18) {
-            System.out.println("buenas tardes");
-        } else {//por lo tanto es false y pasa directo a else
-            System.out.println("buenas noches");
-        }
-        //pero si time fueran las 14 nestro programa imprimiria buenos dias
-
-        //short hand if else
-        //if else normal
-        int time2 = 20;
-        if (time2 < 18) {
-            System.out.println("buenos dias");
-        } else {
-            System.out.println("buenas noches");
+        // for each se utiliza para recorrer elementos de la matriz
+        String[] cars5 = {"Volvo", "BMW", "Ford", "Mazda"};
+        for (String i : cars5) {
+            System.out.println(i);
         }
 
-        //short if else
-        int time3 = 20;
-        String result = (time < 18) ? "buenos dias" : "buenas noches";
-        System.out.println(result);
+        //Matrices multidimencionales
+        int[][] myNumbers = {{1, 2, 3, 4}, {5, 6, 7}};
+        System.out.println(myNumbers[1][2]); // Outputs 7
 
-        //Declaraciones de switch if else
-        int day = 4;
-        switch (day) {
-            case 1:
-                System.out.println("Monday");
-                break;
-            case 2:
-                System.out.println("Tuesday");
-                break;
-            case 3:
-                System.out.println("Wednsday");
-                break;
-            case 4:
-                System.out.println("Thursday");
-                break;
-            case 5:
-                System.out.println("Friday");
-                break;
-            case 6:
-                System.out.println("Saturday");
+        //cambiamos los valores de elementos
+        int[][] myNumbers1 = {{1, 2, 3, 4}, {5, 6, 7}};
+        myNumbers1[1][2] = 9;
+        System.out.println(myNumbers1[1][2]); // Outputs 9 instead of 7
 
-                break;
-            case 7:
-                System.out.println("Sunday");
-                break;//la pabara breack sale del bloque de cambio
-            //lo cual hace que se detenga el codigo y haga la prueba de case
-            //ya que se hace el trabajo de los casos pasa a break
-
+        //for loop en una array
+        //para obtener una matriz bidimencional
+        int[][] myNumbers2 = {{1, 2, 3, 4}, {5, 6, 7}};
+        for (int i = 0; i < myNumbers2.length; ++i) {
+            for (int j = 0; j < myNumbers2[i].length; ++j) {
+                System.out.println(myNumbers2[i][j]);
+            }
         }
+
     }
-}
 
-    
+}
