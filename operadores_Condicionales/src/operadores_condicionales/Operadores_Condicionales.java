d53e0734e12b5c13169e2926a2a283c906ad7a0c
JessGarcia08 <164588337+JessGarcia08@users.noreply.github.com>
2024-04-28 11:28:08 -0600

    update operadores_condicionales

--- operadores_Condicionales/src/operadores_condicionales/Operadores_Condicionales.java
+++ operadores_Condicionales/src/operadores_condicionales/Operadores_Condicionales.java
@@ -6,20 +6,21 @@ public class Operadores_Condicionales {
         var a = 8;
         var valorMinimo = 0;
         var valorMaximo = 10;
-
-        var resultado = a >= 0 && a <= 10;
+        //se compara si a esta dentro del rango del valor  maximo
+        var resultado = a >= 0 && a <= 10;//se realiza la comparacion del valor maximo que es 10
         if (resultado) {
-            System.out.println("dentro del rango");
+            System.out.println("dentro del rango");//por lo tanto esta dentro del rango ya que a = 8
         } else {
             System.out.println("Fuera de rango");
         }
-        
+        //comparamos si la persona tiene dia de descanso con verdadero y falso
         var vacaciones = false;
-        var diaDescanso = false;
+        var diaDescanso = false;//se le da el valor de falso 
         if (vacaciones || diaDescanso){
             System.out.println("el padre puede asistir al juego");
               } else {
             System.out.println("el padre esta ocupado");
+            //ya que ambas variables son falsas nos dara que el padre esta ocupado
         }
     }
 
